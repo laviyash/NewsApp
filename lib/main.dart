31f6eca89a01ff228bf3a764df3ce6366fb3818b
Yash <yashshrivastava28@gmail.com>
2020-07-22 21:40:14 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:newsappp/views/home.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'newsapp',
+    theme: ThemeData(
+      primaryColor: Colors.white,
+    ),
+    home: MyApp(),
+  ));
 }
 
-class Myapp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 10,
+      backgroundColor: Colors.white,
+      image: Image.asset('assets/images/news_icon.png'),
+      loadingText: Text(
+        "FlutterNews",
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      loaderColor: Colors.black12,
+      photoSize: 50.0,
+      navigateAfterSeconds: Home(),
+    );
+  }
+}
+
+/* class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,4 +53,4 @@ class Myapp extends StatelessWidget {
       home: Home(),
     );
   }
-}
+} */
